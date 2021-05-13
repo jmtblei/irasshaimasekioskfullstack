7d2b5b6730b22ebd2aa5c8b2a2e7701a375ce173
jmtblei <47536941+jmtblei@users.noreply.github.com>
2021-05-12 20:59:59 -0700

    deploy prep

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,18 @@ app.use("/api/categories", categoriesRoute);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    app.get("*", (req, res) => {
+        res.sendfile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.send("api running");
+    });
+};
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
